966579c45c51762691e9020266a14c3daef48905
Vikram Dhatarwal <vikk.choudhary06@gmail.com>
2026-09-11 14:22:29 +0530

    Time: 0 ms (100%), Space: 17.6 MB (39.52%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& graph,vector<int>& color, int node){
+        stack<int> s;
+        s.push(node);
+        color[node]=0;
+        while(!s.empty()){
+            int curr_node= s.top();
+            int curr_col= color[curr_node];
+            s.pop();
+            for(int x : graph[curr_node]){
+                if(color[x]==-1){
+                    if(curr_col==0){
+                        
+                        color[x]=1;
+                    }
+                    else{
+                        color[x]=0;
+                    }
+                    s.push(x);
+                }
+                else{
+                    if(color[x]==curr_col) return true;
+                }
+            }
+
+        }
+        return false;
+
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int n= graph.size();
+        vector<int> color(n,-1);
+        for(int i=0;i<n;i++){
+            if(color[i]==-1){
+                if(dfs(graph,color,i)) return false;
+            }
+        }
+        return true;
+
+
+    }
+};
